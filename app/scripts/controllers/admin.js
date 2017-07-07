59266c2eecdfeab1dc900b34931665c5d98ec74d
Alonso <Alonso@iMac-de-Amarello.local>
2017-07-07 18:35:12 -0500

    Admin panel improved.

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -10,11 +10,38 @@
 angular.module('lernApp').controller('AdminCtrl', function ($scope, fireFactory) {
     console.log('AdminCtrl corriendo');
     $scope.step = 0;
+    $scope.point = 0;
+    $scope.read = 0;
+    $scope.question = 0;
+    $scope.option = 0;
     $scope.submit = function() {
       console.log('Submit corriendo');
       fireFactory.setCourse($scope.courseName, $scope.data);
     }
     $scope.addStep = function() {
       $scope.step ++;
+      $scope.point = 0;
+      $scope.read = 0;
+      $scope.question = 0;
+      $scope.option = 0;
+    }
+    $scope.addPoint = function() {
+      $scope.point ++;
+    }
+    $scope.addRead = function() {
+      $scope.read ++;
+    }
+    $scope.addQuestion = function() {
+      $scope.question ++;
+      $scope.option = 0;
+    }
+    $scope.addOption = function() {
+      $scope.option ++;
+    }
+    $scope.backStep = function() {
+      $scope.step --;
+    }
+    $scope.logIt = function() {
+      console.log($scope.data);
     }
   });
